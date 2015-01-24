7095129da56d0c3d0085a9bb9bc40438839b361e
Rene Alfonso <functionr@gmail.com>
2015-01-24 18:42:58 -0500

    Rene_Scene2 Finished
    It works, and if it stops working is because Lazaro changed my
    KillerAxe class.

---
+++ Assets/Scritps/KillerAxe.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillerAxe : MonoBehaviour 
+{
+	private float angles = 20;
+
+	private const int LIMIT = 90;
+
+	bool delayedBool = false;
+
+	private Transform myTransform;
+
+	void Awake()
+	{
+		myTransform = transform;
+	}
+	
+	void Update() 
+	{
+		myTransform.Rotate(0, 0, angles * Time.deltaTime);
+
+		if(myTransform.rotation.eulerAngles.z >= LIMIT && !delayedBool)
+		{
+			myTransform.Rotate(0, 180, 0);
+			
+			setDelayedBool(false);
+		}
+	}
+
+	/// <summary>
+	/// Sets my boolean to a value but with a delay.
+	/// </summary>
+	/// <param name="b">If set to <c>true</c> b.</param>
+	private void setDelayedBool(bool b)
+	{
+		StartCoroutine("setMyBool", b);
+	}
+	
+	private IEnumerator setMyBool(bool b)
+	{
+		delayedBool = !b;
+	
+		yield return new WaitForSeconds(5f);
+	
+		delayedBool = b;
+	}
+}
