9815cb0d48238b01f00d6eec657d5f81336d77be
Lazaro Herrera <lherr012@fiu.edu>
2015-01-24 15:45:46 -0500

    Scene 3 Upgrades
    Rotator script

---
+++ Assets/Scritps/R.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class R : MonoBehaviour {
+
+	public float angles = 40;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((int) this.transform.parent.rotation.eulerAngles.z != 0) 
+			this.transform.parent.Rotate (0, 0, angles * Time.deltaTime);
+		else
+			this.transform.parent.rotation = Quaternion.Euler(new Vector3 (0, 0, 0));
+	}
+}
