19f19bb02557be30dcb8235be582d7067a51707e
Lazaro Herrera <lherr012@fiu.edu>
2015-01-25 06:17:58 -0500

    Added BG Music
    Added BG music and sound effects. Created simple script to play on
    contact.

---
+++ Assets/Scritps/PlayOnAquire.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayOnAquire : MonoBehaviour {
+
+	private AudioSource clip;
+	// Use this for initialization
+	void Start () {
+		clip = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			if(clip != null && !clip.isPlaying)
+			{
+				clip.Play();
+			}
+		}
+	}
+}
