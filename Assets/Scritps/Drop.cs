c083397b782337e1713d86c80da278b8f6fbf967
Lazaro Herrera <lherr012@fiu.edu>
2015-01-25 01:24:24 -0500

    Lazaro Lava Scene 1

---
+++ Assets/Scritps/Drop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drop : MonoBehaviour {
+
+	public float dropSpeed =  5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.parent.Translate(0,dropSpeed*Time.deltaTime, 0);
+	}
+}
