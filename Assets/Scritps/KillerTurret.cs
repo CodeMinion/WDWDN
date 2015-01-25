98016d9160953d1eff4b758b018f2a771a098f51
Lazaro Herrera <lherr012@fiu.edu>
2015-01-24 19:26:20 -0500

    Lazaro Scene 2

---
+++ Assets/Scritps/KillerTurret.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillerTurret : MonoBehaviour {
+
+	public float minAngle = 45f;
+	public float maxAngle = 0f;
+	public float deltaPerSecond =100f;
+
+	public GameObject instantiate;
+	public GameObject instantiateTarget;
+		
+	// Use this for initialization
+	void Start () {
+		maxAngle = 360f - minAngle;
+		Time.timeScale = 0.4f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ((int)Time.timeSinceLevelLoad % 2 == 0) 
+		{
+			GameObject temp = (GameObject) GameObject.Instantiate(instantiate, instantiateTarget.transform.position, instantiate.transform.rotation);
+			Destroy(temp, 20f);
+		}
+
+		if (deltaPerSecond > 0 && 
+			this.transform.parent.localRotation.eulerAngles.y < 180 &&
+		    this.transform.parent.localRotation.eulerAngles.y > minAngle)
+		{
+			deltaPerSecond = -deltaPerSecond;
+			Debug.Log ("Switch");
+		}
+
+		if (deltaPerSecond < 0 && 
+		    this.transform.parent.localRotation.eulerAngles.y > 180 &&
+		    this.transform.parent.localRotation.eulerAngles.y < maxAngle)
+		{
+			deltaPerSecond = -deltaPerSecond;
+			Debug.Log ("Switch");
+		}
+
+		this.transform.parent.Rotate(0,deltaPerSecond * Time.deltaTime, 0);
+	}
+}
---
+++ Assets/Scritps/ReturnTime.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReturnTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1.0f;
+	}
+}
